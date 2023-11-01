53a9d66b6fbd231d5dddc54dd85b6396281d38d3
ihavecoke <lb563@foxmail.com>
2023-11-01 16:24:43 +0800

    Fixed vitepress build window undefeind error

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,12 +1,14 @@
-import DefaultTheme from 'vitepress/theme';
-import '../../style.scss';
-import VPBadge from 'vitepress/dist/client/theme-default/components/VPBadge.vue';
+import DefaultTheme from "vitepress/theme";
+import "../../style.scss";
+import VPBadge from "vitepress/dist/client/theme-default/components/VPBadge.vue";
 const theme = {
   ...DefaultTheme,
   enhanceApp: (ctx) => {
     const { app } = ctx;
-        app.component('Badge', VPBadge);
-        window.WhaleDocs = ctx;
+    app.component("Badge", VPBadge);
+    if (typeof window !== "undefined") {
+      window.WhaleDocs = ctx;
     }
-}
+  },
+};
 export default theme;
