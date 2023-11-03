0f4c8c81f8b9cd033081d76984c3ed66838de26c
ihavecoke <lb563@foxmail.com>
2023-11-03 17:48:53 +0800

    Support images zoom

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,10 @@
 import DefaultTheme from "vitepress/theme";
 import "../../style.scss";
 import VPBadge from "vitepress/dist/client/theme-default/components/VPBadge.vue";
+import mediumZoom from "medium-zoom";
+import { useRoute } from "vitepress";
+import { onMounted, watch, nextTick } from "vue";
+
 const theme = {
   ...DefaultTheme,
   enhanceApp: (ctx) => {
@@ -9,6 +13,21 @@ const theme = {
     if (typeof window !== "undefined") {
       window.WhaleDocs = ctx;
     }
+  },
+  setup() {
+    // Support zooming images
+    const route = useRoute();
+    const initZoom = () => {
+      new mediumZoom(".content-container .main img", { background: "#dddde3" });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom()),
+    );
+
   },
 };
 export default theme;
