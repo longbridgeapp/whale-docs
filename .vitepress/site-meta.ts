d2ddff117ffcccc3d1976ade2dbeb3633276cdb4
mixbo <lb563@foxmail.com>
2023-11-07 16:08:51 +0800

    Add more nav item & change brand color

--- .vitepress/site-meta.ts
+++ .vitepress/site-meta.ts
@@ -3,15 +3,10 @@ const title = "Longbridge Whale docs";
 const description =
   "Whale 為券商提供一站式、開箱即用的產品和解決方案，包含 App、交易、風控、清結算、CRM、智能營銷等前中後台系統，以及行情報價、資訊、基本面等數據服務，同時 Whale 也集成了銀行通道業務、股票通道業務、期權通道業務、基金通道業務、KYC/AML 等服務";
 const url = "https://longbridgewhale.com/zh-HK/docs";
-export const siteMetadata:any = [
+
+export const siteMetadata: any = [
   //   facebook
-  [
-    "meta",
-    {
-      property: "og:type",
-      content: "website",
-    },
-  ],
+  ["meta", { property: "og:type", content: "website" }],
   [
     "meta",
     {
